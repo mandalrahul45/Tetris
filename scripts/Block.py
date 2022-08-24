6375f0d51ce4a3a52514e2702fdad0c0e1413721
mandalrahul45 <mandalrahul5281@gmail.com>
2022-08-24 23:07:54 +0530

    Added Block.py class and made other appropriate changes to other files. FOR: genralization of blocks

---
+++ scripts/Block.py
@@ -0,0 +1,29 @@
+import pygame
+class Block:
+
+    def __init__(self,mi,mj,color):
+        #the vars mi,mj holds the cordinates of central single block in a group of block
+        self.mi = mi
+        self.mj = mj
+        
+        self.color=color
+
+        #other_cordinates  list will be holding the coordinates of single blocks component to form a block
+        #they will be set by the blocks implementing the block class
+        self.other_cordinates = None #expected to have list of lists
+
+class Iblock(Block):
+     
+    def __init__(self, mi, mj, color):
+         super().__init__(mi, mj, color)
+         self.updateOtherCordinates()
+    
+    def updateOtherCordinates(self):
+        toc = [[self.mi,self.mj],
+               [self.mi+1,self.mj],
+               [self.mi+2,self.mj],
+               [self.mi+3,self.mj]]
+        self.other_cordinates=toc    
+    
+    
+
